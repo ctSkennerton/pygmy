3cabb16b215782f6da56f86ed14815522cfbb058
ctSkennerton <c.skennerton@gmail.com>
2015-05-02 20:06:23 -0700

    code cleanup
    remove the boilerplate code from the original Qt example project

--- src/glwidget.h
+++ src/glwidget.h
@@ -112,23 +112,12 @@ public:
     /** Called whenever translation changes. */
     void TranslationChanged() {}
 
-public slots:
-    void setXRotation(int angle);
-    void setYRotation(int angle);
-    void setZRotation(int angle);
-    void cleanup();
-
-signals:
-    void xRotationChanged(int angle);
-    void yRotationChanged(int angle);
-    void zRotationChanged(int angle);
 
 protected:
     void initializeGL() Q_DECL_OVERRIDE;
     void paintGL() Q_DECL_OVERRIDE;
     void resizeGL(int width, int height) Q_DECL_OVERRIDE;
     void mousePressEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
-    void mouseMoveEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
 
 protected:
     /** Tree to be displayed in viewport. */
@@ -141,24 +130,10 @@ protected:
     ViewportOverviewPtr m_overview;
 
 private:
-    void setupVertexAttribs();
 
     bool m_core;
-    int m_xRot;
-    int m_yRot;
-    int m_zRot;
+
     QPoint m_lastPos;
-    Logo m_logo;
-    QOpenGLVertexArrayObject m_vao;
-    QOpenGLBuffer m_logoVbo;
-    QOpenGLShaderProgram *m_program;
-    int m_projMatrixLoc;
-    int m_mvMatrixLoc;
-    int m_normalMatrixLoc;
-    int m_lightPosLoc;
-    QMatrix4x4 m_proj;
-    QMatrix4x4 m_camera;
-    QMatrix4x4 m_world;
     bool m_transparent;
 
     /** Current zoom factor to apply to viewport. */
