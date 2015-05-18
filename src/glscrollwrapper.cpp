660f91bb9695a892600afa9411aa0a9e14282979
ctSkennerton <c.skennerton@gmail.com>
2015-05-17 23:25:38 -0700

    add in the proper signals to the scroll wrapper

--- src/glscrollwrapper.cpp
+++ src/glscrollwrapper.cpp
@@ -1,7 +1,16 @@
 #include "glscrollwrapper.h"
+#include "glwidget.h"
+#include <QResizeEvent>
+#include <QtDebug>
+#include <QScrollBar>
 
-GLScrollWrapper::GLScrollWrapper()
+GLScrollWrapper::GLScrollWrapper(QWidget * parent) : QAbstractScrollArea(parent)
 {
+
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+
+
 
 }
 
@@ -10,3 +19,35 @@ GLScrollWrapper::~GLScrollWrapper()
 
 }
 
+QSize GLScrollWrapper::sizeHint() const
+{
+    return QSize(800, 600);
+}
+
+void GLScrollWrapper::resizeEvent(QResizeEvent * event)
+{
+    qDebug() << "resizing"<<event->size().width()<<" "<<event->size().height()<<" "<<event->oldSize().width()<<" "<<event->oldSize().height();
+    //QAbstractScrollArea::resizeEvent(event);
+    GLWidget * glw = dynamic_cast<GLWidget *>(viewport());
+    glw->resizeEvent(event);
+    //glWidget->resizeGL(event->size().width(), event->size().height());
+}
+
+void GLScrollWrapper::paintEvent(QPaintEvent *event)
+{
+
+
+    //QAbstractScrollArea::paintEvent(event);
+    //glWidget->updateGL();
+    GLWidget * glw = dynamic_cast<GLWidget *>(viewport());
+
+    glw->paintEvent(event);
+}
+
+void GLScrollWrapper::canvasHeight(float height)
+{
+    //qDebug() << __FILE__ <<" "<< __LINE__ << " "<<__PRETTY_FUNCTION__<< " "<<height;
+    verticalScrollBar()->setRange(0, height);
+    //qDebug() << __FILE__ <<" "<< __LINE__ << " "<<__PRETTY_FUNCTION__<< " "<<verticalScrollBar()->maximum();
+
+}
--- src/glscrollwrapper.h
+++ src/glscrollwrapper.h
@@ -1,12 +1,27 @@
 #ifndef GLSCROLLWRAPPER_H
 #define GLSCROLLWRAPPER_H
 
+#include <QWidget>
+#include <QAbstractScrollArea>
 
+
+/** Implements a canvas that enables a GLWidget to be scrolled*/
 class GLScrollWrapper : public QAbstractScrollArea
 {
 public:
-    GLScrollWrapper();
+    GLScrollWrapper(QWidget *parent);
     ~GLScrollWrapper();
+    QSize sizeHint() const Q_DECL_OVERRIDE;
+    void update();
+
+public slots:
+    void canvasHeight(float height);
+
+protected:
+    void resizeEvent(QResizeEvent *);
+    void paintEvent(QPaintEvent *);
+
+private:
 };
 
 #endif // GLSCROLLWRAPPER_H
